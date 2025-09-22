f83d7d8f4182979b78516d6b4cf61d13c4ed7be9
Parvathy <parvathyvasudevan63@gmail.com>
2025-09-22 23:35:36 +0530

    msh

--- task08.js
+++ task08.js
@@ -1,122 +1,122 @@
-// function hai(a,b){
-//     product = a * b;
-//     return product;
-// }
-// mul = hai(10,30);
-// console.log(mul);
-// function jack(a,b,c){
-//     if((a > b) && (a  > c)){
-//         console.log(a, "is the largest");
+// // function hai(a,b){
+// //     product = a * b;
+// //     return product;
+// // }
+// // mul = hai(10,30);
+// // console.log(mul);
+// // function jack(a,b,c){
+// //     if((a > b) && (a  > c)){
+// //         console.log(a, "is the largest");
     
 
-//     }
-//     else if(b > c){
-//         console.log(b, "is thelargest");
-//     }
-//     else {
-//         console.log(c,"is the largest");
-//     }
+// //     }
+// //     else if(b > c){
+// //         console.log(b, "is thelargest");
+// //     }
+// //     else {
+// //         console.log(c,"is the largest");
+// //     }
 
-// }
-// function jack(arr){
-//     let max = arr[0]
-//     for(i = 0;i<arr.length;i++){
-//         if(arr[i]>max){
-//             max = arr[i];
+// // }
+// // function jack(arr){
+// //     let max = arr[0]
+// //     for(i = 0;i<arr.length;i++){
+// //         if(arr[i]>max){
+// //             max = arr[i];
             
           
-//         }
-//     }
-//        return max;
+// //         }
+// //     }
+// //        return max;
     
-// }
+// // }
 
-//     res = jack([10,20,30]);
-//     console.log(res);
-// function vow(name){
-//     let count = 0;
+// //     res = jack([10,20,30]);
+// //     console.log(res);
+// // function vow(name){
+// //     let count = 0;
     
-//     for(let i = 0; i < name.length - 1;i++){
-//         let char = name[i];
-//     if(char==='e' || char == 'i'|| char == "u" || char == "o"|| char =="a"){
-//         count ++;
-//     }
-// }
+// //     for(let i = 0; i < name.length - 1;i++){
+// //         let char = name[i];
+// //     if(char==='e' || char == 'i'|| char == "u" || char == "o"|| char =="a"){
+// //         count ++;
+// //     }
+// // }
 
-//    if(count > 0){
-//         return count;
-//     }
-// }
-// let add = vow("Parvathy");
-// console.log(add);
-// function remove(arr){
-//    let  re=[]
-//     for(var i = 0; i < arr.length;i++){
-//         if(!(re.includes(arr[i])))
-//             {
-//             re.push(arr[i]);
+// //    if(count > 0){
+// //         return count;
+// //     }
+// // }
+// // let add = vow("Parvathy");
+// // console.log(add);
+// // function remove(arr){
+// //    let  re=[]
+// //     for(var i = 0; i < arr.length;i++){
+// //         if(!(re.includes(arr[i])))
+// //             {
+// //             re.push(arr[i]);
         
-//         }
-//     }
-//     console.log(re);
+// //         }
+// //     }
+// //     console.log(re);
 
-// }
-// remove([10,10,34,35,37,10]);
-// function str(name){
-//    var ha="";
-//     for(var i = name.length -1 ;i>=0;i--){
-//         ha= ha + name[i];
-//     }
-//     console.log(ha);
-// }
-// str("PARVATHY");
-// function ret(name){
-//     max=0;
-//     maxchar=0;
+// // }
+// // remove([10,10,34,35,37,10]);
+// // function str(name){
+// //    var ha="";
+// //     for(var i = name.length -1 ;i>=0;i--){
+// //         ha= ha + name[i];
+// //     }
+// //     console.log(ha);
+// // }
+// // str("PARVATHY");
+// // function ret(name){
+// //     max=0;
+// //     maxchar=0;
    
-//     for(var i = 0;i<name.length;i++){
-//         let char = name[i];
-//          let count = 0;
+// //     for(var i = 0;i<name.length;i++){
+// //         let char = name[i];
+// //          let count = 0;
        
-//         for(var j = 0; j < name.length;j++){
+// //         for(var j = 0; j < name.length;j++){
 
        
-//         if(char == name[j]){
-//             count ++;
-//         }
-//     }
+// //         if(char == name[j]){
+// //             count ++;
+// //         }
+// //     }
         
             
         
         
-//         if(count > max){
-//             max = count;
-//             maxchar = char;
+// //         if(count > max){
+// //             max = count;
+// //             maxchar = char;
 
+// //         }
+// //     }
+// //     return maxchar;
+// // }
+// // console.log(ret("prograaaaaaamming"));
+//   function sor(arr){
+//     for(var i = 0; i < arr.length;i++){
+//         for(var j = i + 1; j < arr.length;j++)
+//         {
+//             if(arr[i]>arr[j]){
+//                 temp = arr[i];
+//                 arr[i]= arr[j];
+//                 arr[j] = temp;
+//             }
 //         }
-//     }
-//     return maxchar;
-// }
-// console.log(ret("prograaaaaaamming"));
-  function sor(arr){
-    for(var i = 0; i < arr.length;i++){
-        for(var j = i + 1; j < arr.length;j++)
-        {
-            if(arr[i]>arr[j]){
-                temp = arr[i];
-                arr[i]= arr[j];
-                arr[j] = temp;
-            }
-        }
 
         
-    }
-    console.log(arr);
-    console.log(arr[arr.length-2]);
-    console.log(arr[1])
+//     }
+//     console.log(arr);
+//     console.log(arr[arr.length-2]);
+//     console.log(arr[1])
 
-  }
-  sor([10,20,30,5,8]);
+//   }
+//   sor([10,20,30,5,8]);
 // function val(name){
 //     for(var i = 0 ;i < name.length;i++){
 //         var char = name[i];
@@ -136,3 +136,23 @@
     
 
 // val("PARVATHY");
+// const main = (x) =>{
+//    return  x();
+// }
+// const add = () =>{
+//     let a = 10;
+//     let  b = 20;
+//     sum = a + b;
+//     return sum;
+// }
+// console.log(main(add));
+const main = (x) =>{
+   console.log( x());
+}
+const add = () =>{
+    let a = 10;
+    let  b = 20;
+    sum = a + b;
+    return sum;
+}
+main(add);
---
+++ task09.js
@@ -0,0 +1,35 @@
+// const str = ["apple","banana","graped"];
+// // const ans = str.map((arr) => arr.toUpperCase());
+// // console.log(ans);
+// const leng = (arr) =>{
+//     return (arr.length);
+
+// }
+// const ans = str.map(leng);
+// console.log(ans);
+// const num = [10,34,345,3456];
+// // const dou= (p) =>{
+// //     return p * p;
+// // }
+// // const final = num.map(dou);
+// // console.log(final);
+  
+// const odd = (nu) =>{
+//     if(!(nu % 2 == 0))
+//         return num
+// }
+// const ans = num.filter(odd);
+// console.log(ans);
+const str1 =["apple","banana","spoon","boot"];
+const no =(st) => {
+    if(st.includes('a') )
+        return st;
+}
+const yes = str1.filter(no);
+console.log(yes);
+
+
+
+
+
+
